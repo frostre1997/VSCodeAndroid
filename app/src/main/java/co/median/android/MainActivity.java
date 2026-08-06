63fa563424d983054720dae042a373199126f69d
Frostre_ <n9043395@gmail.com>
2026-08-06 11:20:51 +0200

    Add import for Window class in MainActivity

--- app/src/main/java/co/median/android/MainActivity.java
+++ app/src/main/java/co/median/android/MainActivity.java
@@ -38,6 +38,7 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.view.WindowManager;
