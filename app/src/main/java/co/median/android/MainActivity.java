bac9681f0f7c76a8179a1faf5187d5fc249bdfeb
Frostre_ <n9043395@gmail.com>
2026-08-06 11:06:46 +0200

    Improve system UI visibility setup in MainActivity
    Refactor system UI visibility settings for clarity.

--- app/src/main/java/co/median/android/MainActivity.java
+++ app/src/main/java/co/median/android/MainActivity.java
@@ -229,19 +229,17 @@ public class MainActivity extends AppCompatActivity implements Observer,
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState)
-        
+        super.onCreate(savedInstanceState);
+
         getWindow().getDecorView().setSystemUiVisibility(
-        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-          View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-          View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-          View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-          View.SYSTEM_UI_FLAG_FULLSCREEN
-          View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-    
-            
-        );
-        getWindow().setStatusBarColor(getResources().getColor(android.R.color.transparent));
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+       );
+       getWindow().setStatusBarColor(getResources().getColor(android.R.color.transparent));
         
         
         final AppConfig appConfig = AppConfig.getInstance(this);
